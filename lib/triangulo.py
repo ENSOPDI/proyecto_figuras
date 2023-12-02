22a389212fcd0142ab67f3e89625eb514259d7da
David Mera <david.mera@usc.es>
2023-12-02 10:26:40 +0100

    Añadida funcionalidad triángulo

---
+++ lib/triangulo.py
@@ -0,0 +1,8 @@
+def get_identificador():
+    print("Triangulo")
+
+def get_area(base, altura):
+    return (base*altura)/2
+
+def get_perimetro(lado_a, lado_b, lado_c):
+    return lado_a+lado_b+lado_c
